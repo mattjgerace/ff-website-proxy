0c48bb8334a25b6cc3352f8f7a3b4751a972035d
Matt Gerace <matthewjgerace@gmail.com>
2025-12-22 13:36:53 -0500

    join url

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 
-// Only allow your frontend
 app.use(cors({
   origin: process.env.FRONTEND_URL
 }))
@@ -14,17 +13,17 @@ app.use(cors({
 const BACKEND_URL = process.env.BACKEND_URL
 const BACKEND_API_KEY = process.env.BACKEND_API_KEY
 
-// Proxy route example
-app.all('/api/*splat', async (req, res) => {
+app.all('/*splat', async (req, res) => {
   try {
-    const url = `${BACKEND_URL}${req.path.replace(/^\/api/, '')}`
+    const path = req.params.splat
+    const url = `${BACKEND_URL}/${path.join('/')}`
     
     const response = await axios({
       method: req.method,
       url,
       headers: {
         ...req.headers,
-        'Authorization': `Bearer ${BACKEND_API_KEY}`
+        'Authorization': `${BACKEND_API_KEY}`
       },
       data: req.body
     })
